902c8b28725693db457fc84cf77baec76dad9bb7
nicolachinellato <78468160+nicolachinellato@users.noreply.github.com>
2021-04-14 09:02:01 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -131,18 +131,18 @@ controller.moveSprite(Navetta_spaziale, 100, 100)
 Navetta_spaziale.setStayInScreen(true)
 info.setLife(5)
 info.setScore(0)
-game.onUpdateInterval(randint(200, 500), function () {
+game.onUpdateInterval(randint(400, 700), function () {
     asteroide = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . c c . . . . . . . . 
         . . . . c a f b c . . . . . . . 
-        . . . . b f f b c c . . . . . . 
+        . . . . b f f 9 c c . . . . . . 
         . . . a a f b a b a c . . . . . 
-        . . . c a c b b f f b . . . . . 
+        . . . c a 9 b b f f b . . . . . 
         . . . . b f f b f a b . . . . . 
-        . . . . a f f b b b a . . . . . 
+        . . . . a f f b 9 b a . . . . . 
         . . . . . a b b c c . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -153,24 +153,24 @@ game.onUpdateInterval(randint(200, 500), function () {
     asteroide.setPosition(160, randint(0, 120))
     asteroide.setVelocity(randint(-50, -100), 0)
 })
-game.onUpdateInterval(randint(500, 1000), function () {
+game.onUpdateInterval(randint(700, 1000), function () {
     mySprite = sprites.create(img`
-        . . . . . . . . . c c 8 . . . . 
-        . . . . . . 8 c c c f 8 c c . . 
-        . . . c c 8 8 f c a f f f c c . 
-        . . c c c f f f c a a f f c c c 
-        8 c c c f f f f c c a a c 8 c c 
-        c c c b f f f 8 a c c a a a c c 
-        c a a b b 8 a b c c c c c c c c 
-        a f c a a b b a c c c c c f f c 
-        a 8 f c a a c c a c a c f f f c 
-        c a 8 a a c c c c a a f f f 8 a 
-        . a c a a c f f a a b 8 f f c a 
-        . . c c b a f f f a b b c c 6 c 
-        . . . c b b a f f 6 6 a b 6 c . 
-        . . . c c b b b 6 6 a c c c c . 
-        . . . . c c a b b c c c . . . . 
-        . . . . . c c c c c c . . . . . 
+        . . . . . . . . c c c c . . . . 
+        . . . . c c c c c c c c c . . . 
+        . . . c f c c a 9 a a c a c . . 
+        . . c c f f f f a a a c a a 9 . 
+        . . c c a f f c a a f f f a a c 
+        . . c c a a a a b c f f f a a c 
+        . c c c c a c c b a f c a a c c 
+        c a f f c 9 c a b b 6 b b b c c 
+        c a f f f f c c c 6 b 9 b a a c 
+        c a a c f f c a 6 6 b b b a a c 
+        c c b a a a a b 6 b b a b b 9 . 
+        . c c b b b b b b b a c c b a . 
+        . . c c c b c c c b a a b c . . 
+        . . . . c 9 a c c b b b c . . . 
+        . . . . c b b a a 6 b c . . . . 
+        . . . . . . b 6 6 c c . . . . . 
         `, SpriteKind.Enemy)
     mySprite.setPosition(160, randint(0, 120))
     mySprite.setVelocity(randint(-50, -100), 0)
